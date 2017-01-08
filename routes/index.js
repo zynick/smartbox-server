9a1a07ad274eb8a616b0be8eb0a33e799d477986
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-08 14:34:19 +0800

    refactor code

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,42 @@
 'use strict';
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const secret = require('../config.json').secret;
 const router = express.Router();
+const isProd = express().get('env') === 'production';
+const {
+    version
+} = require('../package.json');
+
 
 router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'SMARTBOX API Server'
-    });
+    res.status(200).json(`SMARTBOX API Server v${version}`);
 });
 
 router.use('/v1', require('./v1'));
+
+
+/* Catch 404 and Forward to Error Handler */
+router.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/* Error Handlers */
+router.use((err, req, res, next) => {
+    let result = {
+        status: err.status || 500,
+        message: err.message || 'Internal Server Error',
+    };
+    // hide stacktrace in production, show otherwise
+    if (!isProd) {
+        result.stack = err.stack;
+    }
+    res
+        .status(result.status)
+        .json({
+            error: result
+        });
+});
 
 module.exports = router;
