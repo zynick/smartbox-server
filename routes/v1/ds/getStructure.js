a45a0cc838b61877010bfa2dd4f0a5826716d94a
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-08 15:48:19 +0800

    query ds information from ha-ds server

--- routes/v1/ds/getStructure.js
+++ routes/v1/ds/getStructure.js
@@ -1,10 +1,45 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 const router = express.Router();
+const {
+    host,
+    port
+} = require('../../../config.json')['ha-ds'];
+const url = `http://${host}:${port}`;
 
-router.get('/', (req, res) => {
-	res.json('hello getStructure');
+const httpGet = (url, next) => {
+    http
+        .get(url, (res) => {
+            let data = '';
+            res
+                .on('data', d => data += d)
+                .on('end', () => {
+                    try {
+                        res.body = JSON.parse(data);
+                    } catch (e) {
+                        res.body = data;
+                    }
+                    next(null, res);
+                })
+                .on('error', next);
+        })
+        .on('error', next)
+        .on('socket', (socket) => {
+            socket.setTimeout(30 * 1000); // 30 seconds;
+            socket.on('timeout', () => next(new Error(`request timeout: ${url}`)));
+        });
+};
+
+
+router.get('/', (req, res, next) => {
+    httpGet(`${url}/v2/getStructure`, (err, httpRes) => {
+        if (err) {
+            return next(err);
+        }
+        res.status(httpRes.statusCode).json(httpRes.body);
+    });
 });
 
 module.exports = router;
